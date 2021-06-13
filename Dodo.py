b8a81930cde605c5219be5a00d13d3ed10f563e5
vivek anand <vivekanandyt@gmail.com>
2021-06-13 14:32:26 +0530

    Voice properties, time update

--- Dodo.py
+++ Dodo.py
@@ -1,2 +1,15 @@
 import pyttsx3 as px
+import datetime
 
+engine= px.init()
+voice=engine.getProperty('voices')
+engine.setProperty('voice', voice[1].id)
+VoiceRate= 185
+engine.setProperty('rate', VoiceRate)
+def pronounce(_input):
+    engine.say(_input)
+    engine.runAndWait()
+pronounce("Vivek")
+def time():
+    cur_time=datetime.datetime.now().strftime("%I,%M, %S")
+    return cur_time
