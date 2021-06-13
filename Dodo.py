e03246f59878f82e32f05201bde8a83f2f16f85c
vivek anand <vivekanandyt@gmail.com>
2021-06-13 16:31:52 +0530

    Date time features added

--- Dodo.py
+++ Dodo.py
@@ -4,12 +4,20 @@ import datetime
 engine= px.init()
 voice=engine.getProperty('voices')
 engine.setProperty('voice', voice[1].id)
-VoiceRate= 185
+VoiceRate= 170
 engine.setProperty('rate', VoiceRate)
 def pronounce(_input):
     engine.say(_input)
     engine.runAndWait()
-pronounce("Vivek")
+pronounce("Hello. This is dodo. How may I help you?")
 def time():
-    cur_time=datetime.datetime.now().strftime("%I,%M, %S")
-    return cur_time
+    cur_time=datetime.datetime.now().strftime("%I,%M, %S") 
+    pronounce(cur_time)
+def date():
+    year=datetime.datetime.now().year
+    month= datetime.datetime.now().month
+    day=datetime.datetime.now().day
+    pronounce("Today's date is ")
+    date_tod="{}, {}, {}".format(day, month, year)
+    pronounce(date_tod)
+
