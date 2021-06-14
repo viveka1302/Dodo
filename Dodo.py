9f2535ee7ffc77aa818126f58c30fffc81a29083
vivek anand <vivekanandyt@gmail.com>
2021-06-14 18:58:34 +0530

    Respond function added, wikipedia search added

--- Dodo.py
+++ Dodo.py
@@ -1,7 +1,9 @@
+from typing import Text
 import pyttsx3 as px
 import datetime
 import speech_recognition as sr
-import tkinter
+import tkinter as tk
+import wikipedia as wk
 
 engine= px.init()
 voice=engine.getProperty('voices')
@@ -11,7 +13,6 @@ engine.setProperty('rate', VoiceRate)
 def pronounce(_input):
     engine.say(_input)
     engine.runAndWait()
-pronounce("Hello. This is dodo. How may I help you?")
 def time():
     cur_time=datetime.datetime.now().strftime("%I,%M, %S") 
     pronounce("The time is ")
@@ -31,11 +32,47 @@ def get_command():
         audio= r.listen(source)
     try:
         print('recognizing...')
-        query = r.recognize_google(audio)
+        query = r.recognize_google(audio, language='en-US')
         print(query)
+        return query
     except Exception as e:
         print(e)
         pronounce("Say that again please...")
         return None
     return query
-get_command()
+def OnClick():
+    x=str(get_command()).lower()
+    inp.config(text=x)
+    if "time" in x:
+        time()
+    if "date" in x:
+        date()
+    elif "quit" or "offline" in x:
+        quit(0)
+    
+    elif "wikipedia" or "wiki" or "look up" or "search":
+        q=x.replace("wikipedia" or "wiki" or "look up" or "search", " ")
+        try:
+            result=str(wk.summary(q, sentences=2))
+            result=result.replace(". ",".\n")
+            reply.config(text=result)
+            pronounce("Here's what I found on wikipedia:")
+            pronounce(result)
+        except Exception as e:
+            print(e)
+            reply.config(text='Whoops! No matching results found')
+            pronounce("No results found")
+    
+    
+window=tk.Tk()
+window.title('Dodo: The smart voice assistant')
+window.geometry('720x360')
+but=tk.Button(window, text="Mic On", fg='blue', command=OnClick)
+but.pack()
+inp=tk.Label(window, text=' ')
+inp.pack()
+reply=tk.Label(window, text='')
+reply.pack()
+pronounce("Hello. This is dodo. How may I help you?")
+window.mainloop()
+    
